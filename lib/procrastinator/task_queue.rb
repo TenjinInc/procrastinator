9f6de694a739a78f4842c95f59511c5c7f8e0f21
Robin Miller <robin@tenjin.ca>
2016-01-27 13:50:48 -0700

    Added some properties to TaskQueue

--- lib/procrastinator/task_queue.rb
+++ lib/procrastinator/task_queue.rb
@@ -1,5 +1,15 @@
 module Procrastinator
    class TaskQueue
-      # TODO: attributes: max_fails, timeout
+      DEFAULT_TIMEOUT      = 3600 # seconds = one hour
+      DEFAULT_MAX_ATTEMPTS = 20
+
+      attr_reader :name, :timeout, :max_attempts
+
+      # Timeout is in seconds
+      def initialize(name: '', max_attempts: DEFAULT_MAX_ATTEMPTS, timeout: DEFAULT_TIMEOUT)
+         @name         = name
+         @timeout      = timeout
+         @max_attempts = max_attempts
+      end
    end
 end
--- spec/task_queue_spec.rb
+++ spec/task_queue_spec.rb
@@ -1,9 +1,45 @@
 require 'spec_helper'
 
 describe Procrastinator::TaskQueue do
-   it 'should have a default sleep time'
-   it 'should have a default max fails'
-   it 'should have a default timeout'
+   describe '#initialize' do
+      it 'should accept a name' do
+         (1..3).each do |name|
+            queue = Procrastinator::TaskQueue.new(name: name)
 
+            expect(queue.name).to eq name
+         end
+      end
+
+      it 'should accept a timeout' do
+         (1..3).each do |t|
+
+            queue = Procrastinator::TaskQueue.new(timeout: t)
+
+            expect(queue.timeout).to eq t
+         end
+      end
+
+      it 'should provide default timeout' do
+         queue = Procrastinator::TaskQueue.new
+
+         expect(queue.timeout).to eq Procrastinator::TaskQueue::DEFAULT_TIMEOUT
+      end
+
+      it 'should accept a max_attempts' do
+         (1..3).each do |t|
+            queue = Procrastinator::TaskQueue.new(max_attempts: t)
+
+            expect(queue.max_attempts).to eq t
+         end
+      end
+
+      it 'should provide default max_attempts' do
+         queue = Procrastinator::TaskQueue.new
+
+         expect(queue.max_attempts).to eq Procrastinator::TaskQueue::DEFAULT_MAX_ATTEMPTS
+      end
+   end
+
+   it 'should have a default checking freq'
 end
 
